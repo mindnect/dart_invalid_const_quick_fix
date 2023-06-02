ad647a11b0bd8c630111da94ad794d8dbdf99622
Ryu G.H <mindnect@gmail.com>
2023-06-02 11:38:42 +0900

    Fix issue that apply twice

--- src/extension.ts
+++ src/extension.ts
@@ -1,93 +1,89 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    // console.log("Extension is now active!");
-    context.subscriptions.push(
-        vscode.languages.registerCodeActionsProvider(
-            { language: "dart", scheme: "file" },
-            new InvalidConstantFixProvider(),
-            {
-                providedCodeActionKinds: [
-                    vscode.CodeActionKind.QuickFix,
-                    vscode.CodeActionKind.SourceFixAll,
-                ],
-            }
-        )
-    );
+  // console.log("Extension is now active!");
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      { language: "dart", scheme: "file" },
+      new InvalidConstantFixProvider(),
+      {
+        providedCodeActionKinds: [vscode.CodeActionKind.QuickFix, vscode.CodeActionKind.SourceFixAll],
+      }
+    )
+  );
 }
 
 class InvalidConstantFixProvider implements vscode.CodeActionProvider {
-    provideCodeActions(
-        document: vscode.TextDocument,
-        range: vscode.Range | vscode.Selection,
-        context: vscode.CodeActionContext,
-        token: vscode.CancellationToken
-    ): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
-        const invalidConstantDiagnostics = context.diagnostics.filter((diagnostic) => {
-            if (typeof diagnostic.code === "object" && "value" in diagnostic.code) {
-                return (
-                    diagnostic.code.value === "const_with_non_const" ||
-                    diagnostic.code.value === "const_with_non_constant_argument"
-                );
-            } else {
-                return diagnostic.code === "invalid_constant";
-            }
-        });
+  private fixedIndex: number[] = [];
 
-        const quickFixActions = invalidConstantDiagnostics
-            .filter((diagnostic) => {
-                if (typeof diagnostic.code === "object" && "value" in diagnostic.code) {
-                    return diagnostic.code.value === "const_with_non_constant_argument";
-                } else {
-                    return diagnostic.code === "invalid_constant";
-                }
-            })
-            .map((diagnostic) =>
-                this.createFix(document, diagnostic, vscode.CodeActionKind.QuickFix)
-            );
-
-        const sourceFixAllActions = invalidConstantDiagnostics.map((diagnostic) =>
-            this.createFix(document, diagnostic, vscode.CodeActionKind.SourceFixAll)
-        );
-
-        return [...quickFixActions, ...sourceFixAllActions];
-    }
-
-    private createFix(
-        document: vscode.TextDocument,
-        diagnostic: vscode.Diagnostic,
-        kind: vscode.CodeActionKind
-    ): vscode.CodeAction {
-        const edit = new vscode.WorkspaceEdit();
-        const action = new vscode.CodeAction("Fix invalid constant", kind);
-
-        action.diagnostics = [diagnostic];
-        const errorRange = diagnostic.range;
-        const errorStartPosition = errorRange.start;
-        const documentText = document.getText();
-
-        // Find the closest 'const' keyword before the error range
-        const constKeyword = "const ";
-        const constIndex = documentText.lastIndexOf(
-            constKeyword,
-            document.offsetAt(errorStartPosition)
-        );
-
-        if (constIndex !== -1) {
-            // Get the text from 'const' to the end of the error range
-            const constRange = new vscode.Range(
-                document.positionAt(constIndex),
-                errorRange.end.translate(0, 1)
-            );
-
-            const constText = document.getText(constRange);
-            // console.log(`Error phrase: ${constText}`);
-
-            // Replace constKeyword of the text with the fixed text
-            const fixedText = constText.replace(constKeyword, "");
-            edit.replace(document.uri, constRange, fixedText);
+  provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range | vscode.Selection,
+    context: vscode.CodeActionContext,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
+    const quickFixActions = context.diagnostics
+      .filter((diagnostic) => {
+        if (typeof diagnostic.code === "object" && "value" in diagnostic.code) {
+          return (
+            diagnostic.code.value === "const_with_non_constant_argument" ||
+            diagnostic.code.value === "const_constructor_with_field_initialized_by_non_const"
+          );
+        } else {
+          return diagnostic.code === "invalid_constant";
         }
-        action.edit = edit;
-        return action;
+      })
+      .map((diagnostic) => this.createFix(document, diagnostic, vscode.CodeActionKind.QuickFix));
+
+    const sourceFixAllActions = context.diagnostics
+      .filter((diagnostic) => {
+        this.fixedIndex = [];
+        if (typeof diagnostic.code === "object" && "value" in diagnostic.code) {
+          return (
+            diagnostic.code.value === "const_with_non_const" ||
+            diagnostic.code.value === "const_with_non_constant_argument" ||
+            diagnostic.code.value === "const_constructor_with_field_initialized_by_non_const"
+          );
+        } else {
+          return diagnostic.code === "invalid_constant";
+        }
+      })
+      .map((diagnostic) => this.createFix(document, diagnostic, vscode.CodeActionKind.SourceFixAll));
+
+    return [...quickFixActions, ...sourceFixAllActions];
+  }
+
+  private createFix(
+    document: vscode.TextDocument,
+    diagnostic: vscode.Diagnostic,
+    kind: vscode.CodeActionKind
+  ): vscode.CodeAction {
+    const edit = new vscode.WorkspaceEdit();
+    const action = new vscode.CodeAction("Fix invalid constant", kind);
+
+    action.diagnostics = [diagnostic];
+    const errorRange = diagnostic.range;
+    const errorStartPosition = errorRange.start;
+    const documentText = document.getText();
+
+    // Find the closest 'const' keyword before the error range
+    const constKeyword = "const ";
+    const constIndex = documentText.lastIndexOf(constKeyword, document.offsetAt(errorStartPosition));
+
+    if (constIndex !== -1 && !this.fixedIndex.includes(constIndex)) {
+      this.fixedIndex.push(constIndex);
+      // Get the text from 'const' to the end of the error range
+      const constRange = new vscode.Range(document.positionAt(constIndex), errorRange.end.translate(0, 1));
+
+      const constText = document.getText(constRange);
+      // console.log(`Error phrase: ${constText}`);
+
+      // Replace constKeyword of the text with the fixed text
+      const fixedText = constText.replace(constKeyword, "");
+      edit.replace(document.uri, constRange, fixedText);
+      action.edit = edit;
     }
+
+    return action;
+  }
 }
